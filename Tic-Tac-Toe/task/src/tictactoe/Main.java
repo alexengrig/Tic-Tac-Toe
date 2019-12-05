71fe47f83305c8106b0caf8c94a0031e6665d5ad
Grig Alex <alexengrigdev@gmail.com>
2019-12-05 18:51:43 +0300

    Add solution

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,14 +1,30 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int size = 3;
-        char[] chars = new char[]{'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'};
-        for (int i = 0; i < chars.length; i++) {
+        Scanner scanner = new Scanner(System.in);
+        char[] chars = scanner.nextLine().toCharArray();
+        printFiled(chars);
+    }
+
+    public static void printFiled(char[] values) {
+        int length = values.length;
+        int size = (int) Math.sqrt(length);
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.print("\n| ");
+        for (int i = 0; i < length; i++) {
             if (i >= size && i % size == 0) {
-                System.out.println();
+                System.out.print("|\n| ");
             }
-            System.out.print(chars[i] + " ");
+            System.out.print(values[i] + " ");
+        }
+        System.out.println("|");
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
         }
     }
 }
