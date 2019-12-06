97a79a32a161adf8641678b9f8991abafcce6fb1
Grig Alex <alexengrigdev@gmail.com>
2019-12-06 22:36:51 +0300

    Add solution

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -7,6 +7,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         char[] chars = scanner.nextLine().toCharArray();
         printFiled(chars);
+        char x = 'X';
+        char o = 'O';
+        if (win(chars, x)) {
+            System.out.println("X wins");
+        } else if (win(chars, o)) {
+            System.out.println("O wins");
+        }
+        if (!hasEmpty(chars) && !win(chars, x) && !win(chars, o)) {
+            System.out.println("Draw");
+        }
+        if (hasEmpty(chars) && !win(chars, x) && !win(chars, o)) {
+            System.out.println("Game not finished");
+        }
+        if (win(chars, x) && win(chars, o) || hasDifferenceCount(chars)) {
+            System.out.println("Impossible");
+        }
     }
 
     public static void printFiled(char[] values) {
@@ -26,5 +42,46 @@ public class Main {
         for (int i = 0; i < length; i++) {
             System.out.print("-");
         }
+        System.out.println();
+    }
+
+    public static boolean win(char[] values, char value) {
+        boolean horizontal = (value == values[0] && value == values[1] && value == values[2])
+                ||
+                (value == values[3] && value == values[4] && value == values[5])
+                ||
+                (value == values[6] && value == values[7] && value == values[8]);
+        boolean vertical = (value == values[0] && value == values[3] && value == values[6])
+                ||
+                (value == values[1] && value == values[4] && value == values[7])
+                ||
+                (value == values[2] && value == values[5] && value == values[8]);
+        boolean diagonal = (value == values[0] && value == values[4] && value == values[8])
+                ||
+                (value == values[2] && value == values[4] && value == values[6]);
+        return horizontal || vertical || diagonal;
+    }
+
+    public static boolean hasEmpty(char[] values) {
+        for (char value : values) {
+            if (value == '_') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasDifferenceCount(char[] values) {
+        int x = 0;
+        int o = 0;
+        for (char value : values) {
+            if (value == 'X') {
+                x++;
+            } else if (value == 'O') {
+                o++;
+            }
+        }
+        int difference = x - o;
+        return difference >= 2 || difference <= -2;
     }
 }
